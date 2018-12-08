7552efa86ce3e4e1b2ebe81b6508b129975c5a51
ccs100203 <ccs100203@gmail.com>
2018-12-08 15:31:03 +0800

    Finish Daylight , Audio is half

---
+++ Assets/Scripts/Daylight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Daylight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        x = transform.rotation.x;
+        y = transform.rotation.y;
+        z = transform.rotation.z;
+
+        transform.Rotate(speed, 0, 0);
+
+
+    }
+
+    static public float x, y, z;
+    private bool open = false;
+    private float _time = 5;
+    public float speed = 1f;
+}
---
+++ Assets/Scripts/MorningMusic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorningMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        bgm1 = GameObject.FindGameObjectWithTag("MusicMorning").GetComponent<AudioSource>();
+        bgm2 = GameObject.FindGameObjectWithTag("MusicNight").GetComponent<AudioSource>();
+        //Debug.Log(bgm1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*if (Daylight.x >= 0 && Daylight.x <= 200)
+        {
+            bgm.UnPause();
+        }
+        else
+        {
+            bgm.Pause();          
+        }*/
+        if (Daylight.x < 0 || Daylight.x > 200)
+        {
+            bgm1.Pause();
+            bgm2.UnPause();
+        }
+        else
+        {
+            bgm1.UnPause();
+            bgm2.Pause();
+        }
+    }
+
+    private AudioSource bgm1,bgm2;
+}
